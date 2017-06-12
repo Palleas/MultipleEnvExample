b52a1baf4ab56e595861b2a8f2acfc458dcd9f7d
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-12 15:59:10 -0400

    Create buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+
+# Adding environment variables into the Info.plist using PlistBuddy
+# More infos on Plistbuddy: https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man8/PlistBuddy.8.html
 /usr/libexec/PlistBuddy -c "Add SERVER_ENDPOINT String $SERVER_ENDPOINT" "$BUDDYBUILD_WORKSPACE/MultipleEnvExample/Info.plist"
 /usr/libexec/PlistBuddy -c "Add STRIPE_KEY String $STRIPE_KEY" "$BUDDYBUILD_WORKSPACE/MultipleEnvExample/Info.plist"
